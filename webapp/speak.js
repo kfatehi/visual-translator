7a4382dabf955d61cd5585861a638bcf29691d59
Keyvan Fatehi <keyvanfatehi@gmail.com>
2021-07-24 06:04:47 +0000

    update speech url

--- webapp/speak.js
+++ webapp/speak.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = async function speak(content) {
-    let resp = await axios.post("http://8.3.29.110:3000/tts", { content });
-    return "http://8.3.29.110:3000"+resp.data.path;
+    let resp = await axios.post("https://tts.farsi.keyvan.tv/tts", { content });
+    return "https://tts.farsi.keyvan.tv"+resp.data.path;
 }
 
